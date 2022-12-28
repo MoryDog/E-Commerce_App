012222623fe6d699ccf9fdb2911da2064e8368dd
MoryDog <s3881103@rmit.edu.vn>
2022-12-28 22:21:21 +0700

    Initial commit

---
+++ app/src/main/java/rmit/ad/e_commerce_app/AddProduct.java
@@ -0,0 +1,14 @@
+package rmit.ad.e_commerce_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addproduct);
+    }
+}
