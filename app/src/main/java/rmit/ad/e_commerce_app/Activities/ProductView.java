9b44171d708291d4e031a9b97572d51f705807df
MoryDog <s3881103@rmit.edu.vn>
2023-01-16 01:01:21 +0700

    add product view activity

---
+++ app/src/main/java/rmit/ad/e_commerce_app/Activities/ProductView.java
@@ -0,0 +1,16 @@
+package rmit.ad.e_commerce_app.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import rmit.ad.e_commerce_app.R;
+
+public class ProductView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_view);
+    }
+}
