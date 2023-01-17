fda7afd91ce1df7024cc95d0717bb70493b72923
huynguyen2842 <gianghuy2804@gmail.com>
2023-01-18 00:05:20 +0700

    Add Product Information, create category activity, create favorite fragment

--- app/src/main/java/rmit/ad/e_commerce_app/Adapter/ViewHolder.java
+++ app/src/main/java/rmit/ad/e_commerce_app/Adapter/ViewHolder.java
@@ -5,6 +5,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import rmit.ad.e_commerce_app.R;
@@ -13,11 +14,13 @@ public class ViewHolder extends RecyclerView.ViewHolder {
     ImageView product_image;
     TextView product_name;
     TextView product_price;
+    CardView parent;
 
     public ViewHolder(@NonNull View itemView) {
         super(itemView);
         product_image = itemView.findViewById(R.id.product_img);
         product_name = itemView.findViewById(R.id.product_name);
         product_price = itemView.findViewById(R.id.product_price);
+        parent = itemView.findViewById(R.id.parent);
     }
 }
