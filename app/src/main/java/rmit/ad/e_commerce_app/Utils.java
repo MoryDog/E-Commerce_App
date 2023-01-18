59b966b3d4ba330c49ac720b0e1528de6bdf0737
minhboro197 <minhvuviet2000@gmail.com>
2023-01-18 15:33:23 +0700

    small bug

--- app/src/main/java/rmit/ad/e_commerce_app/Utils.java
+++ app/src/main/java/rmit/ad/e_commerce_app/Utils.java
@@ -21,7 +21,7 @@ public class Utils {
 
 
     public Utils() {
-        /*
+
         if (Products == null){
             Products = new ArrayList<>();
         }
@@ -29,7 +29,7 @@ public class Utils {
             ShoeProducts = new ArrayList<>();
             SetShoesData();
         }
-         */
+
     }
 
 
