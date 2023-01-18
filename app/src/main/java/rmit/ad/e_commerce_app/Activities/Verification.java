d788b7f1d09576548bb0fb7199b5fbcc11d62fc3
tranhoangv698@gmail.com <tranhoangv698@gmail.com>
2023-01-19 01:09:02 +0700

    adding Register

--- app/src/main/java/rmit/ad/e_commerce_app/Activities/Verification.java
+++ app/src/main/java/rmit/ad/e_commerce_app/Activities/Verification.java
@@ -1,22 +1,31 @@
 package rmit.ad.e_commerce_app.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import rmit.ad.e_commerce_app.HttpClasses.HttpHandler;
 import rmit.ad.e_commerce_app.R;
 
 public class Verification extends AppCompatActivity {
     EditText CodeField;
     Button ConfirmButton;
     View snackbar_view;
+
+    String username;
+    String confirm_code;
+    String jsonString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +36,44 @@ public class Verification extends AppCompatActivity {
         ConfirmButton = findViewById(R.id.ConfirmButton);
         snackbar_view = findViewById(android.R.id.content);
 
+        Intent intent = getIntent();
+        username = intent.getExtras().get("username").toString();
+
+
+
         ConfirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Verification.this, Login.class );
+                confirm_code = CodeField.getText().toString();
+                new doVerification().execute();
+            }
+        });
+    }
+
+    private class doVerification extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            jsonString = HttpHandler.postVerification("http://54.151.194.4:3000/confirm", username, confirm_code);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            System.out.println("-----------------------------");
+            System.out.println(jsonString);
+            if (jsonString.equals("Confirm successfully")) {
+                Intent intent = new Intent(Verification.this, Login.class);
                 Snackbar.make(snackbar_view, "Account Verified! Enter your credentials to login)", Snackbar.LENGTH_SHORT).show();
                 startActivity(intent);
             }
-        });
+            else {
+                Toast.makeText(Verification.this, jsonString, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+        }
+
     }
 }
