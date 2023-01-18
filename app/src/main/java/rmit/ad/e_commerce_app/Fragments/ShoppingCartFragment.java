0f6f2c53f3c7527e178e28852363d37a0cd4c71b
MoryDog <s3881103@rmit.edu.vn>
2023-01-19 01:40:31 +0700

    delete shopping cart activity, edit shopping cart fragment

--- app/src/main/java/rmit/ad/e_commerce_app/Fragments/ShoppingCartFragment.java
+++ app/src/main/java/rmit/ad/e_commerce_app/Fragments/ShoppingCartFragment.java
@@ -3,12 +3,17 @@ package rmit.ad.e_commerce_app.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import rmit.ad.e_commerce_app.Adapter.ProductAdapter;
 import rmit.ad.e_commerce_app.R;
+import rmit.ad.e_commerce_app.Utils;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,7 +21,7 @@ import rmit.ad.e_commerce_app.R;
  * create an instance of this fragment.
  */
 public class ShoppingCartFragment extends Fragment {
-
+    ProductAdapter adapter;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -61,6 +66,15 @@ public class ShoppingCartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+        View root = inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+        RecyclerView recyclerView1 = root.findViewById(R.id.shoppingCart_rec);
+//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getContext(), 2);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView1.setLayoutManager(linearLayoutManager);
+        adapter = new ProductAdapter(this.getContext());
+        adapter.SetUpProducts(Utils.obtainInstance().getAllProducts());
+        recyclerView1.setAdapter(adapter);
+
+        return root;
     }
 }
