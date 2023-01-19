6e6ac8f8ffac4968e5f33f7a91a3b744bdf091f5
huynguyen2842 <gianghuy2804@gmail.com>
2023-01-19 16:58:22 +0700

    modify add button layout

--- app/src/main/java/rmit/ad/e_commerce_app/Adapter/ViewHolder.java
+++ app/src/main/java/rmit/ad/e_commerce_app/Adapter/ViewHolder.java
@@ -2,6 +2,7 @@ package rmit.ad.e_commerce_app.Adapter;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,7 +17,7 @@ public class ViewHolder extends RecyclerView.ViewHolder {
     TextView product_name;
     TextView product_price;
     CardView parent;
-    TextView DeleteBtn;
+    Button DeleteBtn;
 
     public ViewHolder(@NonNull View itemView) {
         super(itemView);
